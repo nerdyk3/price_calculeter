6bac946e0e198f355b5ae46bd1307d7e1fbc29d3
coderk3 <kunalkumar.kus@gmail.com>
2018-07-09 11:04:27 +0530

    token generated

--- mySite/price_calculator/price_cal/urls.py
+++ mySite/price_calculator/price_cal/urls.py
@@ -7,5 +7,5 @@ app_name = 'price_cal'
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('', views.userPrice, name='userPrice'),
+	#path('', views.userPrice, name='userPrice'),
 ]
--- mySite/price_calculator/price_cal/views.py
+++ mySite/price_calculator/price_cal/views.py
@@ -1,9 +1,10 @@
 
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, render_to_response
+from django.http import HttpResponse, Http404,HttpResponseRedirect,HttpResponseNotFound
 from django.utils import timezone
 from .models import cal
 from .forms import priceForm
+from django.template import Context,loader,RequestContext
 # Create your views here.
 
 def index(request):
@@ -13,28 +14,42 @@ def index(request):
 			TotalDay=userDate()
 			if TotalDay<30:
 				user_price=cal.objects.price-(cal.objects.price*0.06)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+
 			elif TotalDay>31 or TotalDay<90:
 				user_price=cal.objects.price-(cal.objects.price*0.12)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+
 			elif TotalDay>91 or TotalDay<210:
 				user_price=cal.objects.price-(cal.objects.price*0.25)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+
 			elif TotalDay>211 or TotalDay<365:
 				user_price=cal.objects.price-(cal.objects.price*0.50)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+
 			elif TotalDay>366 or TotalDay<1095:
 				user_price=cal.objects.price-(cal.objects.price*0.62)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+
 			elif TotalDay>1096 or TotalDay<2555:
 				user_price=cal.objects.price-(cal.objects.price*0.75)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+
 			elif TotalDay>2556 or TotalDay<4380:
 				user_price=cal.objects.price-(cal.objects.price*0.95)
-				userPrice(user_price)
+				#userPrice(user_price)
+				return render_to_response('index.html',context_instance=RequestContext(request,{'user_price':user_price}))
+		return render(request,'price_cal/index.html',{'form':form})
 	else:
 		form = priceForm()
-	return render(request,'price_cal/index.html',{'form':form})
+		return render(request,'price_cal/index.html',{'form':form})
 
 def userDate(request):
 	isValid = False
@@ -50,5 +65,5 @@ def userDate(request):
 	diff= g-d
 	return(diff)
 
-def userPrice(user_price):
-	return render_to_response('result.html',context_instance=RequestContext(request,{'user_price':user_price}))
+#def userPrice(user_price):
+	#return render_to_response('result.html',context_instance=RequestContext(request,{'user_price':user_price}))
